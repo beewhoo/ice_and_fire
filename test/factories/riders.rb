1cbe17777e71086985ae29bec23ce4ec315daba8
beewhoo <bibeksinghthapa@gmail.com>
2018-07-05 13:10:54 -0400

    completed

--- test/factories/riders.rb
+++ test/factories/riders.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :rider do
+    name 'Aegon'
+    nickname 'The Conqueror'
+    sequence(:email) { |n| "#{name}#{n}@westeros.com" }
   end
 end
--- test/models/dragon_test.rb
+++ test/models/dragon_test.rb
@@ -19,51 +19,50 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_young_dragons_from_asoiaf
     # use your knowledge of factories to make this test pass!
-    young_dragon = nil
+    young_dragon = build(:dragon, age:3)
 
     # not allowed to change the assert statement!
-    skip unless young_dragon
     assert_equal( 'A Song of Ice and Fire', young_dragon.period_alive)
   end
 
   def test_that_middle_dragons_from_targ_dynasty
     # use your knowledge of factories to make this test pass!
-    middle_dragon = nil
+    middle_dragon = build(:dragon, age:20)
 
+         
     # not allowed to change the assert statement!
-    skip unless middle_dragon
     assert_equal( 'Targaryen Dynasty', middle_dragon.period_alive)
   end
 
   def test_that_ancient_dragons_from_age_of_valyria
     # use your knowledge of factories to make this test pass!
-    ancient_dragon = nil
+    ancient_dragon = build(:dragon, age:500)
+
 
     # not allowed to change the assert statement!
-    skip unless ancient_dragon
+
     assert_equal( 'Age of Valyria', ancient_dragon.period_alive)
   end
 
   def test_that_stranger_cannot_mount
     # use your knowledge of factories to make this test pass!
-    rider = nil
-    dragon = nil
+    rider1 = build(:rider, name:'Aegon')
+    rider2 = build(:rider, name:'Kevin')
+    dragon = build(:dragon, name:'Balerion', rider:rider2)
 
     # not allowed to change the assert statement!
-    skip unless rider && dragon
-    assert_equal("Balerion bucks off Aegon!", dragon.mount(rider))
+    assert_equal("Balerion bucks off Aegon!", dragon.mount(rider1))
   end
 
   def test_that_the_right_rider_can_mount
     # use your knowledge of factories to make this test pass!
     # Note: you'll need to do more than just supply the right names...
     # Hint: you can pass variables as properties too, not just strings!
-    rider = nil
-    dragon = nil
+    rider1 = build(:rider, name:'Aegon')
+    dragon = build(:dragon, name:'Balerion', rider:rider1)
 
     # not allowed to change the assert statement!
-    skip unless rider && dragon
-    assert_equal("Aegon soars into the air on Balerion's back!", dragon.mount(rider))
+    assert_equal("Aegon soars into the air on Balerion's back!", dragon.mount(rider1))
   end
 
 end
